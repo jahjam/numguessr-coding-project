677468c2f53c67f73b3dc834e359a8ba25a7f889
dreamingOfSoy <dreamingofsoy@gmail.com>
2022-02-01 23:59:16 +0000

    Third Commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Selectors
 // Page Text Elements
 const randomNumber = document.querySelector('.random-number');
 const statusText = document.querySelector('.status-text');
@@ -18,86 +19,96 @@ const closeModal = document.querySelector('.modal-close');
 // Modal
 const rulesModal = document.querySelector('.modal');
 
+// Functions
+// Initialize Values
 const init = function () {
-  // init input
+  // init check button & input activation
+  input.disabled = false;
+  checkButton.disabled = false;
+
+  // init input style
   input.style.backgroundColor = 'transparent';
 
   // init focus
   input.focus();
 
-  // init Value
-  highscoreText.textContent = 0;
+  // init values
   scoreText.textContent = 10;
-  input.value = '';
   statusText.textContent = 'Start guessing...';
+  input.value = '';
+  randomNumber.textContent = '?';
 
-  // Functions
-  // Function to generate random number between 1 and 20
-  const generateRandomNumber = function () {
-    const num = Math.ceil(Math.random() * 20);
-    console.log(num);
-    return num;
-  };
-
-  // Function to lower score
-  const lowerScore = function () {
-    curScore--;
-    return curScore;
-  };
-
-  // Values
-  const randomNumberGen = generateRandomNumber();
-  let curScore = 10;
-
-  checkButton.addEventListener('click', function () {
-    console.log(input.value);
-
-    if (input.value === '' || input.value === 0) {
-      statusText.textContent = `Please input a number between 0 and 20`;
-      input.focus();
-      return;
-    }
-
-    if (+input.value === randomNumberGen) {
-      checkButton.disabled = true;
-      randomNumber.textContent = randomNumberGen;
-      statusText.innerHTML = `Yipee! <br/> ${input.value} is right!`;
-      highscoreText.textContent = curScore;
-      return;
-    }
-
-    if (+input.value < randomNumberGen) {
-      lowerScore();
-      scoreText.textContent = curScore;
-      statusText.innerHTML = `Oops! <br/> ${input.value} is too low...`;
-      input.value = '';
-      input.focus();
-    }
-
-    if (+input.value > randomNumberGen) {
-      lowerScore();
-      scoreText.textContent = curScore;
-      statusText.innerHTML = `Oops! <br/> ${input.value} is too high...`;
-      input.value = '';
-      input.focus();
-    }
-
-    if (curScore === 0) {
-      checkButton.disabled = true;
-      statusText.innerHTML = `Oh no! <br/> You lost!`;
-      input.disabled = true;
-      input.style.backgroundColor = '#333';
-    }
-  });
+  // Reset Score
+  curScore = 10;
 };
-init();
 
+// Function to generate random number between 1 and 20
+const generateRandomNumber = function () {
+  const num = Math.ceil(Math.random() * 20);
+  return num;
+};
+
+// Function to lower score
+const lowerScore = function () {
+  curScore--;
+  return curScore;
+};
+
+// Function to calculate outcome of check
+const calculateOutcome = function () {
+  if (input.value === '' || +input.value === 0) {
+    statusText.textContent = `Please input a number between 0 and 20`;
+    input.focus();
+    input.value = '';
+    return;
+  }
+
+  if (+input.value === randomNumberGen) {
+    checkButton.disabled = true;
+    randomNumber.textContent = randomNumberGen;
+    statusText.innerHTML = `Yipee! <br/> ${input.value} is right!`;
+    highscoreText.textContent = curScore;
+    input.value = '';
+    return;
+  }
+
+  if (+input.value < randomNumberGen || +input.value > randomNumberGen) {
+    console.log(input.value);
+    lowerScore();
+    scoreText.textContent = curScore;
+    statusText.innerHTML = `Oops! <br/> ${input.value} is too ${
+      +input.value < randomNumberGen ? 'low' : 'high'
+    }...`;
+    input.focus();
+    return;
+  }
+
+  if (curScore === 0) {
+    console.log(curScore);
+    checkButton.disabled = true;
+    statusText.innerHTML = `Oh no! <br/> You lost!`;
+    input.disabled = true;
+    input.style.backgroundColor = '#333';
+    input.value = '';
+    return;
+  }
+  return;
+};
+
+// Function to toggle modal
+const toggleModal = function () {
+  rulesModal.classList.contains('hidden')
+    ? rulesModal.classList.remove('hidden')
+    : rulesModal.classList.add('hidden');
+};
+
+// Number values
+const randomNumberGen = generateRandomNumber();
+let curScore = 10;
+
+// Event listeners
+window.addEventListener('load', init);
+checkButton.addEventListener('click', calculateOutcome);
 playAgainButton.addEventListener('click', init);
-
-rulesButton.addEventListener('click', function () {
-  rulesModal.classList.remove('hidden');
-});
-
-closeModal.addEventListener('click', function () {
-  rulesModal.classList.add('hidden');
-});
+rulesButton.addEventListener('click', toggleModal);
+closeModal.addEventListener('click', toggleModal);
